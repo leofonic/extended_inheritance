c7e98bf7441eb995b1855d5df8b8ded1c652e47c
HTMH <info@htmh.de>
2016-04-17 02:18:51 +0200

    Angepasst für OXID CE 4.9.7 DE
    Das Modul ist vorkonfiguriert für die Vererbung des Gewichts und der
    Lieferzeit an die Varianten.

---
+++ oxid_4.9.7/copy_this/extended_inheritance/metadata.php
@@ -0,0 +1,20 @@
+<?php
+$aModule = array(
+    'id'          => 'extended_inheritance',
+    'title'       => 'Extended Inheritance',
+    'description' =>  array(
+        'de'=>'Erlaubt Felder vom Vater an Varianten zu vererben',
+        'en'=>'Allows to inherit fields from parent to variant',
+    ),
+    'version'     => '1.1',
+    'url'         => 'http://zunderweb.de',
+    'email'       => 'info@zunderweb.de',
+    'author'      => 'Zunderweb',
+    'extend'      => array(
+        'oxarticle'  => 'extended_inheritance/core/extended_inheritance_oxarticle',
+    ),
+    'settings'    => array(
+        array('group' => 'main', 'name' => 'aDoubleCopyFields', 'type' => 'arr',  'value' => array('oxarticles__oxweight','oxarticles__oxmindeltime','oxarticles__oxmaxdeltime',)),
+        array('group' => 'main', 'name' => 'aInheritFields', 'type' => 'arr',  'value' => array('')),
+    )
+);
